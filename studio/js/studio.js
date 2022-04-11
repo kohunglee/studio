c8a24f4b190c1e8f7497b57a93d300748688c609
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-04-11 13:08:42 +0800

    先写了写文件基础操作的前端逻辑

--- studio/js/studio.js
+++ studio/js/studio.js
@@ -1,21 +1,21 @@
 /**
- * 工作室 js 运行库
+ * 工作室插件 js 运行库
  */
 
     // 定义 ACE 代码编辑器对象
-    let emAceEditor = ace.edit("code"),
-        editor = emAceEditor,
+    const emAceEditor = ace.edit("code"),
+          editor      = emAceEditor,
 
     // 定义图片文件后缀名的正则样式
-        img_patt = new RegExp(/^.+\.(gif|jpg|jpeg|bmp|png|ico|svg)/),
+            img_patt = new RegExp(/^.+\.(gif|jpg|jpeg|bmp|png|ico|svg)/);
 
     // 定义文件在编辑器中的打开模式
-        st_file_type;
+    let st_file_type;
 
     /**
      * 调试
      * 
-     * @param string data 输出的内容
+     * @param {string} data 输出的内容
      */
     function d(data){
         console.log(data)
@@ -24,17 +24,52 @@
     /**
      * 显示消息
      * 
-     * @param string msg 消息内容
+     * @param {string} msg 消息内容
      */
     function msg(msg){
         $("#em_stdio_msg").html(msg);
     }
 
     /**
-     * 编辑器整体覆盖式的插入代码
+     * 控制显示文件操作区 。
+     * 控制哪些操作项显示，哪些操作项隐藏。
      * 
-     * @param string code 插入的内容
-     * @param string type 以什么格式插入
+     * @param {string} type 文件类别。
+     * type 参数有 空 、"file" 、 "folder" 、 "html"
+     */
+    function st_displayFileControl(type){
+        $(".c-link").hide()
+        switch (type) {
+            case "file":
+                $(".c-file").show()
+                break;
+            case "folder":
+                $(".c-folder").show()
+                break;
+            case "html":
+                $(".c-html").show()
+                break;
+            default:
+                $(".c-index").show()
+                break;
+        }
+    }
+
+    /**
+     * 无刷新更新 url 参数 openFile 。
+     * 可在网页刷新时或通过含该参数的 url，方便地打开某个文件
+     * 
+     * @param {string} openFileUrl 消息内容
+     */
+    function st_refleshUrlParam(openFileUrl){
+        // history.pushState('','', "plugin.php?plugin=studio&xxopenFile=..%2FPastePlugin%2FPastePlugin_setting.php..%2FPastePlugin%2FPastePlugin_setting.php&openFile=" + openFileUrl);
+    }
+
+    /**
+     * 编辑器整体覆盖式地插入代码
+     * 
+     * @param {string} code 插入的内容
+     * @param {string} type 以什么格式插入
      */
     function inAce(code,type){
         emAceEditor.getSession().setMode("ace/mode/" + type);
@@ -55,8 +90,8 @@
 	function st_saveCode(url,code){
 		$.post("../content/plugins/studio/studio_control.php?act=saveCode",
 		{
-            url:url,
-            code:code
+            url : url,
+            code: code
 		},
 		function(data){
             d("控制回调：\n"+data);
@@ -75,7 +110,7 @@
     /**
      * 渲染目录列表的 <li> 标签
      * 
-     * @param string array 转换后的文件列表 json 数据
+     * @param {string} array 转换后的文件列表 json 数据
      */
     function st_randerLi(list_json){
         let htmlC = '';
@@ -95,18 +130,18 @@
                 typeIcon = `icofont-penalty-`;
 
                 switch(nameL){
-                    case 'php':
-                    case 'html':
+                    case 'php' : 
+                    case 'html': 
                         typeIcon = `icofont-page`;
                         break;
-                    case 'txt':
-                    case 'md':
+                    case 'txt': 
+                    case 'md' : 
                         typeIcon = `icofont-pencil-alt-5`;
                         break;
-                    case 'js':
+                    case 'js': 
                         typeIcon = `icofont-comment`;
                         break;
-                    default:
+                    default: 
                         typeIcon = `icofont-penalty-card`;
                         break;
                 }
@@ -118,9 +153,9 @@
                 typeIcon = `<span class="file_icon ${typeIcon}"></span>`;
             }
 
-                htmlC     = htmlC + `<li class="file_list_li" onclick="st_activeFile($(this))">`
+                htmlC = htmlC + `<li class="file_list_li" onclick="st_activeFile($(this))">`
                 
-                          + `${typeIcon}<a class="file_link" title="${name}" file_type="${type}" link="${path}" onclick="st_openFile($(this))" href="javascript:void(0);">${name}</a>`
+                          + `${typeIcon}<a class="file_link" id="fileLink" title="${name}" file_type="${type}" link="${path}" onclick="st_openFile($(this))" href="javascript:void(0);">${name}</a>`
                           
                           + "</li>" ;
             }
@@ -131,14 +166,14 @@
     /**
      * 打开项目动作
      * 
-     * @param string url 要打开的文件链接
+     * @param {string} url 要打开的文件链接
      */
 	function st_openProject(url){
 		msg("打开项目中");
         
 		$.post("../content/plugins/studio/studio_control.php?act=openProject",
 		{
-            url:url
+            url: url
 		},
 		function(data){
             d("打开项目，传入的 php 回来的 data：\n"+data);
@@ -148,11 +183,21 @@
 
             htmlC += st_randerLi(list_json);
             htmlC += '</ul>';
-
-            htmlC += `<div class="st_file_control">123</div>`;
+            // 添加操作区 
+            htmlC += `<div class="st_file_control" id="stControl"><hr>
+            <a href="javascript:void(0);" class="c-link c-file c-folder" id="st_removeFile">删除</a>
+            <a href="javascript:void(0);" class="c-link  c-folder c-index" id="st_addFile">新建文件</a>
+            <a href="javascript:void(0);" class="c-link c-folder c-index" id="st_addFolder">新建文件夹</a>
+            <a href="javascript:void(0);" class="c-link c-file c-folder" id="st_renameFile">重命名</a>
+            <a href="javascript:void(0);" class="c-link c-file c-folder" id="st_copyFile">复制</a>
+            <a href="javascript:void(0);" class="c-link c-file c-folder" id="st_cutFile">剪切</a>
+            <a href="javascript:void(0);" class="c-link c-folder c-index" id="st_pasteFile">粘贴</a>
+            <a href="javascript:void(0);" class="c-link st_c_html" id="st_renameFile">在新窗口打开</a>
+            </div>`;
 
             $("#file_list_body").html(htmlC);
             st_toggleFileProj();
+            st_displayFileControl(); // 默认显示 class 为 "c-index" 的项
 		});
 
         msg("");
@@ -187,7 +232,7 @@
             
             $.post("../content/plugins/studio/studio_control.php?act=openFolder",
             {
-                url:url
+                url: url
             },
             function(data) {
                 d("打开项目，控制回调：\n"+data);
@@ -207,7 +252,7 @@
 
             msg("");
             
-        // -- end
+        // -- 如果是文件夹 (end)
         
         }else{
             
@@ -223,18 +268,18 @@
             }
 
             switch(type){
-                case 'txt':
-                case 'md':
+                case 'txt': 
+                case 'md' : 
                     type = "markdown";
                     break;
-                case 'js':
+                case 'js': 
                     type = "javascript";
                     break;
-                case 'css':
-                case 'html':
-                case 'php':
+                case 'css' : 
+                case 'html': 
+                case 'php' : 
                     break;
-                default:
+                default: 
                     type = "markdown";  // 默认使用 markdown 语言模式
                     break;
             }
@@ -243,15 +288,16 @@
 
             $.post("../content/plugins/studio/studio_control.php?act=openCode",
                 {
-                    url:url
+                    url: url
                 },
                 function(data){
                     d("打开文件，控制回调：\n"+data);
                     inAce(data,type);
+                    st_refleshUrlParam(url);
                     $("#saveCode").attr("code_url",url);
                 });
                 
-        // -- end
+        // -- 如果是文件 (end)
         
 	    }
     }
@@ -274,16 +320,32 @@
         // 如果 this 元素是已经打开的目录，则退出
         if($this.children("ul").length > 0) return
 
-        if(event.shiftKey !== 1){
-            // 如果 shift 键没按下
+        if(event.shiftKey !== true){
+            // -- 如果 shift 键没按下 --
+
+        }else{
+            // -- 如果 shift 键按下了 --
+
+            // 兼容部分浏览器，尝试不显示文本选中那些文件条目
             $("html").toggleClass("trans_select");
             window.getSelection().removeAllRanges();
             $("html").toggleClass("trans_select");
-        }else{
-            // 如果
         }
 
         $this.toggleClass("file_active_border");
+
+        /**
+         * 临时测试一下单个文件（夹）的效果
+         */
+        // 获取文件类型和文件地址
+        let fileType = $this.children("#fileLink").attr("file_type")
+        let fileUrl = $this.children("#fileLink").attr("link")
+
+        // 记录激活文件信息
+        $("#stControl").attr("link",fileUrl);
+
+        // 显示操作区
+        st_displayFileControl(fileType);
         
     }
 
@@ -292,7 +354,7 @@ $(document).ready(function(){
 
     /* -----------  ace 编辑器配置  ----------------- */
     //设置默认风格和语言
-    let st_theme = ($(".em_studio_dark").length > 0) ? 'tomorrow_night' : 'clouds',
+    let st_theme    = ($(".em_studio_dark").length > 0) ? 'tomorrow_night' : 'clouds',
         st_language = "markdown";
 
     emAceEditor.setTheme("ace/theme/" + st_theme);
@@ -314,33 +376,25 @@ $(document).ready(function(){
     ace.require("ace/ext/language_tools");
     emAceEditor.setOptions({
         enableBasicAutocompletion: true,
-        enableSnippets: true,
-        scrollPastEnd:1,
-        useSoftTabs:true,
-        enableLiveAutocompletion: true
+        enableSnippets           : true,
+        scrollPastEnd            : 1,
+        useSoftTabs              : true,
+        enableLiveAutocompletion : true
     });
     
     /* ----------- ace 编辑器配置结束  ----------------- */
 
     $("#file_list_body").toggle();
 
-    // 点击保存按钮
-    $("#saveCode").click(function(){
-        let url     = $("#saveCode").attr("code_url"),
-		    code	= getAce();
-
-        d(url)
-
-		st_saveCode(url,code);
-	});
+    /* ----------- JS 事件监听开始  ----------------- */
 
     // 按快捷键保存
     document.addEventListener('keydown', function(e){  // 快捷键
 		if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)){
             e.preventDefault();
 
-            let url     = $("#saveCode").attr("code_url"),
-                code	= getAce();
+            let url  = $("#saveCode").attr("code_url"),
+                code = getAce();
 
             d(url)
 
@@ -348,7 +402,17 @@ $(document).ready(function(){
 		}
 	});
 
-    // 点击转换主题
+    // 点击保存按钮
+    $("#saveCode").click(function(){
+        let url  = $("#saveCode").attr("code_url"),
+            code = getAce();
+
+        d(url)
+
+        st_saveCode(url,code);
+    });
+
+    // 点击转换工作室显示主题
     $("#changeTheme").click(function(){
 
         if($(".em_studio_dark").length > 0){
@@ -370,10 +434,12 @@ $(document).ready(function(){
 
         // 添加页面的离开提示
         window.onbeforeunload = function (e) {
-            e = e || window.event;
+            e                    = e || window.event;
             if (e) e.returnValue = '离开页面提示';
             return '离开页面提示';
         }
     });
-    
+
+    /* ----------- JS 事件监听结束  ----------------- */
 })
+
--- studio/studio_setting.php
+++ studio/studio_setting.php
@@ -1,20 +1,25 @@
 <?php
     // 检查 cookie 信息，以确定是使用 *日间主题* 还是 *夜间主题*
-    if($_COOKIE["em_studio_theme"] === 'dark'){
+    if(isset($_COOKIE["em_studio_theme"]) && $_COOKIE["em_studio_theme"] === 'dark'){
         echo '<html class="em_studio_dark">';
     }
+
+    // 定义版本号，以方便资源引入的缓存更新
+    $st_ver = 100;
+    $st_ver = rand(2,100);
 ?>
-<link rel="stylesheet" type="text/css" href="../content/plugins/studio/css/style.css">
+<link rel="stylesheet" type="text/css" href="../content/plugins/studio/css/style.css?=<?= $st_ver ?>">
 <?php
 !defined('EMLOG_ROOT') && exit('access deined!');
+
 !(ROLE == ('admin' || 'founder')) && exit('非管理员权限！');
 
 require_once 'class/fileSystem.class.php';
 
 function plugin_setting_view()
 {
+    // 定义插件根目录，并通过 filels 类读取所有插件的信息
     $path = "../content/plugins";
-    
     $dir = new filels();
     $file_list = $dir->odir($path);
 
@@ -38,11 +43,12 @@ function plugin_setting_view()
             if (!empty($file_list)):
 				foreach ($file_list as $value):
                     if($value['fileType'] == "file"){continue;}
+                    
+             //<!-- html 正文-->
 			?>
-            <!-- html 正文-->
                 <li>项目名：<a class="project_link" link="<?= $value['filePath'] ?>" onclick="st_openProject($(this).attr('link'))" href="javascript:void(0);"><?= $value['fileName'] ?></a></li>
-			<!--（结束）html 正文-->
             <?php
+            //<!--（结束）html 正文-->
 				endforeach;
 			else:
 			?>
@@ -55,9 +61,9 @@ function plugin_setting_view()
     <a hidden onclick="window.location.href = 'plugin.php';" style="position:fixed" href="#">返回插件页</a>
 </div>
 <!-- 导入 ace 编辑器的 js 库 -->
-<script src="../content/plugins/studio/ace/ace.js" type="text/javascript" charset="utf-8"></script> 
-<script src="../content/plugins/studio/ace/ext-language_tools.js" type="text/javascript" charset="utf-8"></script>
+<script src="../content/plugins/studio/ace/ace.js?=<?= $st_ver ?>" type="text/javascript" charset="utf-8"></script> 
+<script src="../content/plugins/studio/ace/ext-language_tools.js?=<?= $st_ver ?>" type="text/javascript" charset="utf-8"></script>
 <!-- 导入工作室的 js -->
-<script src="../content/plugins/studio/js/studio.js" type="text/javascript" charset="utf-8"></script>
+<script src="../content/plugins/studio/js/studio.js?=<?= $st_ver ?>" type="text/javascript" charset="utf-8"></script>
 <?php
 }
